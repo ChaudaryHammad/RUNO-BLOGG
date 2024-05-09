fe104e96176776e629f5ed801971765f96175a60
ChaudaryHammad <ch.hammadbrw01@gmail.com>
2024-05-10 01:05:37 +0500

    profile done

--- frontend/src/component/Profile.js
+++ frontend/src/component/Profile.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function Profile() {
+  const [data,setData] = useState({}) 
+  useEffect(()=>{
+    const fetchData = async()=>{
+      await axios.get('http://localhost:8000/api/v2/user',{
+        withCredentials:true
+      }).then((res)=>{
+        
+    
+        setData(res.data)
+        console.log(data)
+      
+      }).catch((error)=>{
+        console.log(error)
+      } )
+    }
+    fetchData()
+  },[])
+
   return (
-    <div>Profile</div>
-  )
+    <div className='flex  flex-col  items-center justify-center h-[50vh] w-full border border-red-500'>
+    
+    {
+      data.user ? (
+        <div>
+         <div  className='text-[20px] font-bold cursor-pointer ' style={{fontFamily:"League Spartan"}}>User Name: {data.user.username}</div>
+          <div  className='text-[20px] font-bold cursor-pointer ' style={{fontFamily:"League Spartan"}}>User Email: {data.user.email}</div>
+          <div  className='text-[20px] font-bold cursor-pointer ' style={{fontFamily:"League Spartan"}}>User Id: {data.user._id}</div>
+
+
+        </div>
+      ):(
+        <div>loading...</div>
+      )
+    }</div>
+  ) 
 }
 
 export default Profile
