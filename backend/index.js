2bf9ebe0ee637188c5f1ebba1cd15de4fb21f886
ChaudaryHammad <ch.hammadbrw01@gmail.com>
2024-09-24 13:23:27 +0500

    deployemnet init

--- backend/index.js
+++ backend/index.js
@@ -1,59 +1,51 @@
-const express = require('express')
-const app = express()
-const connectDB = require('./database/connection')
-const cors = require('cors')
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const blog = require('./routes/blogRoutes')
-const user = require('./routes/userRoutes')
+const express = require("express");
+const app = express();
+const connectDB = require("./database/connection");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const blog = require("./routes/blogRoutes");
+const user = require("./routes/userRoutes");
 
 const cloudinary = require("cloudinary");
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({
+    path: "./config/.env",
+  });
+}
 
+const corsConfig = {
+  origin: process.env.Client_URL,
+  credentials: true,
+  method: ["GET", "POST", "PUT", "DELETE"],
+};
 
-app.use(cors(
-    {credentials: true,
-        origin: 'http://localhost:3000'
-        
-    }
-))
-
+app.options("", cors(corsConfig));
+app.use(cors(corsConfig));
 app.use(cookieParser());
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
-
-
-require('dotenv').config({
-    path:'./config/.env'
-})
-
-
-
-
-app.get('/',(req,res)=>{
-    res.send('Hello World')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 // routes defined
 
-app.use('/api/v2/blog',blog)
-app.use('/api/v2/user',user)
-connectDB()
+app.use("/api/blog", blog);
+app.use("/api/user", user);
+connectDB();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
-})
-
-
-
-app.listen(process.env.PORT,()=>{
-    try {
-        console.log(`Server is running on port ${process.env.PORT}`)
-    } catch (error) {
-        console.log(error);
-    }
-
-})
+app.listen(process.env.PORT, () => {
+  try {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,1 +1,1 @@
-export const backend_url = "http://localhost:8000/api/v2";
+export const backend_url = "http://localhost:8000/api";
